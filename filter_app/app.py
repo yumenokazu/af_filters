fdf428367f751d0b467ee915163a718de8c0dc1d
yumenokazu <yumenokazu@users.noreply.github.com>
2018-03-24 22:36:52 +0300

    Add import statement for scheduler

--- filter_app/app.py
+++ filter_app/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 # app.config.from_object('config.ProductionConfig')
 # app.config.from_envvar('APPLICATION_SETTINGS', silent = True) # load another config if variable is EXPORTED
+
 db = SQLAlchemy(app)
 
 from filter_app import views, models
+from filter_app.gsheets import price_task
 
 if __name__ == '__main__':
-    app.run()
+
+    app.run(use_reloader=False)
+
+
+
