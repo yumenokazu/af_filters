e0efa3c5e646642bc145c67dd87d38782302252f
yumenokazu <yumenokazu@users.noreply.github.com>
2018-06-02 22:50:53 +0300

    Add model for league and natural_pk method

--- filter_app/models.py
+++ filter_app/models.py
@@ -1,8 +1,9 @@
-import re
 from datetime import datetime
+from decimal import Decimal
+import re
+from typing import List, Tuple
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import CheckConstraint, exists, and_, func
+from sqlalchemy import CheckConstraint, exists, and_, func, extract
 from filter_app.app import db
 
 
@@ -10,6 +11,10 @@ class Base(db.Model):
     __abstract__ = True
 
     def exists(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def natural_pk():
         raise NotImplementedError
 
 
@@ -43,6 +48,10 @@ class Item(Base):
     def exists(self):
         return db.session.query(exists().where(and_(Item.name == self.name, Item.note == self.note))).scalar()
 
+    @staticmethod
+    def natural_pk():
+        return ['name', 'note']
+
     def __repr__(self):
         return "<Item(name=%s, base=%s, type=%s, chaos=%s, note=%s)>" % (self.name, self.base,
                                                                        self.item_type, self.chaos, self.note)
@@ -71,6 +80,10 @@ class Category(Base):
     def exists(self):
         return db.session.query(exists().where(Category.name == self.name)).scalar()
 
+    @staticmethod
+    def natural_pk():
+        return ['name']
+
     def __repr__(self):
         return "<Category(id='%s', name='%s', link='%s', quantity='%s', class_name='%s')>" % (
             self.id, self.name, self.link, self.quantity, self.class_name)
@@ -80,7 +93,7 @@ class Category(Base):
 class AffixType(Base):
     __tablename__ = 'affix_type'
 
-    def __init__(self, name, filter_name, is_percent, top="max"):
+    def __init__(self, name, filter_name, top="max"):
         self.name = name
         self.filter_name = filter_name
         self.top = top
@@ -89,10 +102,16 @@ class AffixType(Base):
     name = db.Column(db.String, unique=True)  # affix name
     filter_name = db.Column(db.String)  # affix abbr as in filter file
     top = db.Column(db.String(3), CheckConstraint('top.in_(["max", "min"])', name='check_top'))  # minimize or maximize
-    affixes = db.relationship('Affix', backref='affix_type')  # one affix type can have many affixes
+    affixes = db.relationship('Affix', backref='affix_type', lazy='joined')  # one affix type can have many affixes
 
     def exists(self):
-        return db.session.query(exists().where(AffixType.name == self.name)).scalar()
+        #return db.session.query(exists().where(AffixType.name == self.name)).scalar()
+        pk = {k: getattr(self, k) for k in self.natural_pk()} # natural pk keys and values
+        return db.session.query(AffixType.query.filter_by(**pk).exists()).scalar()
+
+    @staticmethod
+    def natural_pk():
+        return ['name']
 
     def __repr__(self):
         return "<AffixType(id='%s', name='%s', filter_name='%s', top='%s')>" % (
@@ -116,8 +135,13 @@ class Affix(Base):
     affix_max = db.Column(db.Numeric(10,5))  # max affix value
 
     def exists(self):
+        # :TODO: rewrite with dict
         return db.session.query(exists().where(and_(Affix.item_id == self.item_id,
                                                     Affix.af_text == self.af_text))).scalar()
+
+    @staticmethod
+    def natural_pk():
+        return ['item_id', 'af_text']
 
     def af_text_shortener(self, repl=None) -> str:
         if repl is None:
@@ -125,16 +149,22 @@ class Affix(Base):
         letters = re.compile(r'[^a-zA-Z ]')  # everything except a-zA-z and space
         return letters.sub(repl, self.af_text).strip()
 
+    def af_text_ranges(self) -> List[Tuple[str, str]]:
+        return [tuple(map(Decimal, t)) for t in re.findall(r"\(([0-9.]+)-([0-9.]+)\)", self.af_text, re.IGNORECASE)]
+
     def __repr__(self):
         return "<Affix(id='%s', item_id='%s', af_text='%s', affix_id='%s', affix_min='%s', affix_max='%s')>" % (
             self.id, self.item_id, self.af_text, self.affix_id, self.affix_min, self.affix_max)
 
 
-# default settings for chaos and percentage, first priority
+# default settings for chaos and percentage
 class GeneralSettings(Base):
     __tablename__ = 'general_settings'
 
-    def __init__(self, user_id, affix_pct, chaos_thr):
+    default_threshold = 5
+    default_pct_settings = 90
+
+    def __init__(self, user_id: int, affix_pct: int, chaos_thr: int):
         self.user_id = user_id
         self.affix_pct = affix_pct
         self.chaos_threshold = chaos_thr
@@ -144,8 +174,16 @@ class GeneralSettings(Base):
     affix_pct = db.Column(db.Integer)  # default value for all affixes
     chaos_threshold = db.Column(db.Integer)  # threshold for price, check lower-priced items
 
+    @classmethod
+    def get_by_user(cls, user_id):
+        return cls.query.filter_by(user_id == user_id).one_or_none()
+
     def exists(self):
         pass
+
+    @staticmethod
+    def natural_pk():
+        return ['user_id']
 
     def __repr__(self):
         return "<GeneralSettings(id='%s', user_id='%s', affix_pct='%s', chaos_threshold='%s')>" % (
@@ -169,6 +207,10 @@ class AffixSettings(Base):
     def exists(self):
         pass
 
+    @staticmethod
+    def natural_pk():
+        return ['user_id', 'affix_id']
+
     def __repr__(self):
         return "<AffixSettings(id='%s', user_id='%s', affix_id='%s', affix_pct='%s')>" % (
             self.id, self.user_id, self.affix_id, self.affix_pct)
@@ -189,10 +231,15 @@ class ItemSettings(Base):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))  # item
+    is_checked = db.Column(db.Boolean) # true if item should be included in filter anyway
     affix_pct = db.Column(db.String)  # affix pcts for item, comma-separated or JSON
 
     def exists(self):
         pass
+
+    @staticmethod
+    def natural_pk():
+        return ['user_id', 'item_id']
 
     def __repr__(self):
         return "<ItemSettings(id='%s', user_id='%s', item_id='%s', affix_pct='%s')>" % (
@@ -208,6 +255,10 @@ class User(Base):
 
     def exists(self):
         pass
+
+    @staticmethod
+    def natural_pk():
+        return ['nick']
 
     def __repr__(self):
         return "<User(id='%s', nick='%s')>" % (
@@ -226,15 +277,57 @@ class Price(Base):
     item_id = db.Column(db.Integer, db.ForeignKey("item.id"),
                         nullable=False)  # use .item to access associated object of class Item
     price = db.Column(db.Numeric(10,2))  # price in chaos
-    date = db.Column(db.DateTime, server_default=func.now()) # server date on insert
+    date = db.Column(db.DateTime, server_default=func.now()) # server datetime on insert
 
     def exists(self):
         return db.session.query(exists().where(and_(Price.item_id == self.item_id,
-                                                    func.DATE(Price.date) == datetime.now().date()))).scalar()
+                                                    func.DATE(Price.date) == datetime.now().date(),
+                                                    extract('hour', Price.date) == datetime.now().hour))).scalar()
+
+    @staticmethod
+    def natural_pk():
+        return ['item_id', 'date']
 
     def __repr__(self):
         return "<Price(id='%s', item_id='%s', price='%s', date='%s')>" % (
             self.id, self.item_id, self.price, self.date)
 
 
+# stores leagues with start and end dates
+class League(Base):
 
+    def __init__(self, name, start, end, main):
+        self.name=name
+        self.start = start
+        self.end = end
+        self.main = main
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    start = db.Column(db.Date, nullable=False)
+    end = db.Column(db.Date, nullable=True)
+    main = db.Column(db.Boolean, nullable=False) # true if league, false if event
+
+    def is_current(self):
+        today = datetime.datetime.now().date()
+        if (today > self.start.date()) and (today < self.end.date()):
+            return True
+        else:
+            return False
+
+    def is_hardcore(self):
+        if self.name[-3:] == ' HC':
+            return True
+        else:
+            return False
+
+    def exists(self):
+        return db.session.query(exists().where(League.name == self.name)).scalar()
+
+    @staticmethod
+    def natural_pk():
+        return ['name']
+
+    def __repr__(self):
+        return "<League(id='%s', name='%s', start='%s', end='%s', main='%s')>" % (
+            self.id, self.name, self.start, self.end, self.main)
